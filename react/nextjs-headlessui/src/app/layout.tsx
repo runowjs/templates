16021bd7790e8c016a260c6671e3698589f2f5f9
shulksteve <shulk.work@gmail.com>
2025-01-22 11:03:21 +0800

    feat: add react-nextjs-headlessui

---
+++ react/nextjs-headlessui/src/app/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from 'next';
+import React from 'react';
+import './globals.css';
+
+export const metadata: Metadata = {
+  title: 'React + Next.js + HeadlessUI',
+  description: 'Powered by Runow',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
