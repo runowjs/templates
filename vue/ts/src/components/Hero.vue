03e7933e386e842bc5004b97c7f17d9550a699bf
shulksteve <shulk.work@gmail.com>
2025-01-23 15:00:52 +0800

    chore: update vue-ts

--- vue/ts/src/components/Hero.vue
+++ vue/ts/src/components/Hero.vue
@@ -2,7 +2,13 @@
 
 <template>
   <a href="https://react.dev" target="_blank">
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="@/assets/vue.svg"
+      width="125"
+      height="125"
+    />
   </a>
   <h1>Vue + TypeScript</h1>
   <p>Powered by <a href="https://runow.dev" target="_blank"> Runow </a></p>
