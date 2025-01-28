6fa3aa12167163fc8344a12e1733f4d4fbcc9b91
shulksteve <shulk.work@gmail.com>
2025-01-28 10:18:14 +0800

    update: react-shadcnui

--- react/nextjs-shadcnui/src/app/(root)/layout.tsx
+++ react/nextjs-shadcnui/src/app/(root)/layout.tsx
@@ -1,5 +1,44 @@
+import AppSidebar from '@/app/(root)/components/app-sidebar';
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from '@/components/ui/breadcrumb';
+import { Separator } from '@/components/ui/separator';
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from '@/components/ui/sidebar';
 import React from 'react';
 
 export default function Layout({ children }: { children?: React.ReactNode }) {
-  return <>{children}</>;
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex sticky top-0 bg-background h-16 shrink-0 items-center gap-2 border-b px-4">
+          <SidebarTrigger className="-ml-1" />
+          <Separator orientation="vertical" className="mr-2 h-4" />
+          <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem className="hidden md:block">
+                <BreadcrumbLink href="#">
+                  Building Your Application
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator className="hidden md:block" />
+              <BreadcrumbItem>
+                <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
+        </header>
+        <main className="w-full flex-auto flex flex-col">{children}</main>
+      </SidebarInset>
+    </SidebarProvider>
+  );
 }
